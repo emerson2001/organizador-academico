64a4090229d08773ce9adec0a12038bdd6581024
Emerson2001 <136753688+Emerson2001@users.noreply.github.com>
2023-12-07 22:58:28 -0300

    Atualização

---
+++ app/model/works.js
@@ -0,0 +1,8 @@
+export class Works {
+    constructor(tipo, titulo, descrição, data) {
+        this.tipo = tipo;
+        this.titulo = titulo;
+        this.descrição = descrição;
+        this.data = data;
+    }
+}
